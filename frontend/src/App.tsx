ff872abc52bee9ae7849aa60ec34e12183d9b423
hacker-flavio <theflavioherrera@gmail.com>
2023-12-08 15:53:53 -0800

    added hacks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,25 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const alertUser = () => {
+    alert("you have been hacked");
+  };
   return (
     <div className="App">
       <div>Hello World, This is Flavio Herrera</div>
+
+      <div>
+        <div>
+          Click the DownLoad Button to download source code for tic tac toe
+        </div>
+
+        <br />
+        <div>↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓</div>
+        <button id="totally not a virus 😳" onClick={() => alertUser()}>
+          DownLoad Source Code
+        </button>
+        <div>↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑</div>
+      </div>
     </div>
   );
 }
