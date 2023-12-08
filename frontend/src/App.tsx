a4ba0113d1ab0c0c2766698d8b202c94d059df2b
hacker-flavio <theflavioherrera@gmail.com>
2023-12-08 14:32:58 -0800

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hello World, This is Flavio Herrera</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const app = express();
+
+const port = process.env.PORT || 4050;
+app.use(express.json());
+
+// Serve static files from the "build" directory
+app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+
+// Handle requests for the root URL
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./frontend/build", "index.html"));
+});
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
